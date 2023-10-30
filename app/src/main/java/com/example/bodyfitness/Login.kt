c4e4582b483c53e1dbb271b053e1267f6b13073c
Himanshu <himanshury1231@gmail.com>
2023-10-31 02:26:22 +0530

    Project connected with FireBase. User can login and signup with email and password.

--- app/src/main/java/com/example/bodyfitness/Login.kt
+++ app/src/main/java/com/example/bodyfitness/Login.kt
@@ -1,11 +1,14 @@
 package com.example.bodyfitness
 
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.bodyfitness.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
     private var binding: ActivityLoginBinding?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,9 +16,47 @@ class Login : AppCompatActivity() {
         binding= ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
+        firebaseAuth= FirebaseAuth.getInstance()
+
         binding?.loginButton?.setOnClickListener {
-            val intent= Intent(this,Signup::class.java)
-            startActivity(intent)
+//            val intent= Intent(this,Signup::class.java)
+//            startActivity(intent)
+
+            login()
         }
+    }
+
+    private fun login()
+    {
+        val email= binding?.loginEmail?.text.toString().trim()
+        val password= binding?.loginPassword?.text.toString().trim()
+
+        //Validation code for correct email, password and confirmPassword.
+
+        if(email.isEmpty() || password.isEmpty() ) {
+
+            Toast.makeText(this,"Please fill all details", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+
+            Toast.makeText(this,"Enter valid email address", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this)
+            {
+                if(it.isSuccessful)
+                {
+                    Toast.makeText(this,"User Login successfully!!!",Toast.LENGTH_SHORT).show()
+                }
+                else
+                {
+                    Toast.makeText(this,"Unable to Login!!!",Toast.LENGTH_SHORT).show()
+                }
+            }
+
     }
 }
--- app/src/main/java/com/example/bodyfitness/Signup.kt
+++ app/src/main/java/com/example/bodyfitness/Signup.kt
@@ -2,10 +2,81 @@ package com.example.bodyfitness
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.bodyfitness.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Signup : AppCompatActivity() {
+
+    //Firebase authentication object is created
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private var binding: ActivitySignupBinding?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding=ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        //Initializing firebaseAuth variable onCreate
+        firebaseAuth= FirebaseAuth.getInstance()
+
+        //Connecting button event and user creation function
+        binding?.continueButton?.setOnClickListener{
+            signupUser()
+        }
+
+
+
+    }
+    //Function is created to signup user via email.
+    private fun signupUser()
+    {
+        val userEmail= binding?.signupEmail
+        val userPassword = binding?.signupPassword
+        val userConfirmPassword = binding?.signupConfirmPassword
+
+        val email=userEmail?.text.toString().trim()
+        val password= userPassword?.text.toString().trim()
+        val confirmPassword= userConfirmPassword?.text.toString().trim()
+
+
+        //Validation code for correct email, password and confirmPassword.
+
+        if(email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+
+            Toast.makeText(this,"Please fill all details",Toast.LENGTH_SHORT).show()
+            return
+        }
+        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+
+            Toast.makeText(this,"Enter valid email address",Toast.LENGTH_SHORT).show()
+            return
+        }
+         if ( confirmPassword != password) {
+             Toast.makeText(this,"Password does not matched",Toast.LENGTH_SHORT).show()
+             return
+        }
+
+
+
+        //Creating user with email and password with firebase
+        //This function will create user with email and password
+        //We will add an listener to check the call is completed or not.
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this)
+            {
+                if(it.isSuccessful)
+                {
+                    //User is created.
+                    Toast.makeText(this,"Congratulations!!User created successfully.",Toast.LENGTH_SHORT).show()
+                    //Code to be further executed.
+                }
+                else
+                {
+                    //Account not created.
+                    Toast.makeText(this,"Error in creating user",Toast.LENGTH_SHORT).show()
+                }
+            }
+
     }
 }
